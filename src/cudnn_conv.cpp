ad5aba5055537432621b681fb4e6759da374084b
薛纪令 <jiling.xjl@alibaba-inc.com>
2019-01-03 20:06:20 +0800

    CudnnConv done

---
+++ src/cudnn_conv.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include "basic.h"
+#include "cudnn_conv.h"
+
+using namespace std;
+
+const size_t kMaxCudnnWorkspace_ = 1024 * 1024 * 10;
+int kMaxDataSize                 = 1024 * 1024 * 1024;
+int kMaxWeightSize               = 7 * 7 * 512 * 512;
+cudnnHandle_t cudnn_handle_g;
+
+CudnnConv::CudnnConv(int in_n, int in_c, int in_h, int in_w,
+              int k_n, int k_c, int k_h, int k_w,
+              int p_h, int p_w,
+              int s_h, int s_w,
+              int d_h, int d_w,
+              int group,
+              cudnnDataType_t in_type,
+              cudnnDataType_t weight_type,
+              cudnnDataType_t out_type,
+              cudnnTensorFormat_t in_format,
+              cudnnTensorFormat_t weight_format,
+              cudnnTensorFormat_t output_format)
+        : input_n_(in_n),input_c_(in_c), input_h_(in_h), input_w_(in_w),
+          kernel_n_(k_n), kernel_c_(k_c), kernel_h_(k_h), kernel_w_(k_w),
+          pad_h_(p_h), pad_w_(p_w),
+          stride_h_(s_h), stride_w_(s_w),
+          dilation_h_(d_h), dilation_w_(d_w),
+          group_(group),
+          input_type_(in_type), weight_type_(weight_type), output_type_(out_type),
+          input_format_(in_format), weight_format_(weight_format), output_format_(output_format),
+          input_data_(nullptr),
+          weight_data_(nullptr),
+          output_data_(nullptr),
+          cudnn_workspace_(nullptr){
+        CHECK_EXIT(group_ != 1, "only support group == 1 now");
+        CHECK_EXIT(in_c != kernel_c_, "in_c != kernel_c_");
+        // set algo 
+        cudnnStatus_t sts;
+        sts = cudnnCreateTensorDescriptor(&input_desc_);
+        CHECK_EXIT(sts != CUDNN_STATUS_SUCCESS, "cudnnCreateTensorDescriptor");
+        sts = cudnnCreateTensorDescriptor(&output_desc_);
+        CHECK_EXIT(sts != CUDNN_STATUS_SUCCESS, "cudnnCreateTensorDescriptor");
+        sts = cudnnCreateFilterDescriptor(&weight_desc_);
+        CHECK_EXIT(sts != CUDNN_STATUS_SUCCESS, "cudnnCreateTensorDescriptor");
+        sts =cudnnCreateConvolutionDescriptor(&conv_desc_);
+        CHECK_EXIT(sts != CUDNN_STATUS_SUCCESS, "cudnnCreateConvolutionDescriptor");
+        sts = cudnnSetTensor4dDescriptorEx(input_desc_,
+                                           input_type_,
+                                           input_n_,
+                                           input_c_,
+                                           input_h_,
+                                           input_w_,
+                                           input_c_ * input_h_ * input_w_,
+                                           input_h_ * input_w_,
+                                           input_w_,
+                                           1);
+        CHECK_EXIT(sts != CUDNN_STATUS_SUCCESS, "cudnnSetTensor4dDescriptorEx");
+        sts = cudnnSetTensor4dDescriptorEx(output_desc_,
+                                           output_type_,
+                                           output_n(),
+                                           output_c(),
+                                           output_h(),
+                                           output_w(),
+                                           output_c() * output_h() * output_w(),
+                                           output_h() * output_w(),
+                                           output_w(), 
+                                           1);
+        CHECK_EXIT(sts != CUDNN_STATUS_SUCCESS, "cudnnSetTensor4dDescriptorEx");
+        sts = cudnnSetConvolution2dDescriptor(conv_desc_, 
+                                              pad_h_,
+                                              pad_w_,
+                                              stride_h_,
+                                              stride_w_,
+                                              dilation_h_,
+                                              dilation_w_,
+                                              CUDNN_CROSS_CORRELATION,
+                                              conv_type());
+        CHECK_EXIT(sts != CUDNN_STATUS_SUCCESS, "cudnnSetConvolution2dDescriptor");
+        sts = cudnnSetFilter4dDescriptor(weight_desc_, 
+                                         weight_type_,
+                                         weight_format_,
+                                         kernel_n_,
+                                         kernel_c_,
+                                         kernel_h_,
+                                         kernel_w_);
+
+
+}
+
+cudnnDataType_t CudnnConv::conv_type() {
+    if ((input_type_ == CUDNN_DATA_FLOAT) &&
+        (output_type_ == CUDNN_DATA_FLOAT) &&
+        (weight_type_ == CUDNN_DATA_FLOAT)) {
+        return CUDNN_DATA_FLOAT;
+    } else {
+        CHECK_EXIT(true, "conv_type not support");
+    }
+}
+
+void CudnnConv::InitAlgo() {
+    cudnnStatus_t sts;
+    sts = cudnnGetConvolutionForwardAlgorithm(cudnn_handle_, 
+                                              input_desc_,
+                                              weight_desc_,
+                                              conv_desc_,
+                                              output_desc_,
+                                              CUDNN_CONVOLUTION_FWD_PREFER_FASTEST,
+                                              0,
+                                              &algo_);
+    CHECK_EXIT(sts != CUDNN_STATUS_SUCCESS, "cudnnGetConvolutionForwardAlgorithm");
+}
+void CudnnConv::Run() {
+    cout << "Run................." << endl;
+    cudnnStatus_t sts;
+//    sts = cudnnConvolutionForward();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,70 @@
 #include <iostream>
+#include "cudnn_conv.h"
+
+void* input_data_g_ = nullptr;
+void* output_data_g_ = nullptr;
+void* weight_data_g_ = nullptr;
+void* cudnn_workspace_g_ = nullptr;
+
+
 using namespace std;
+
+void TestCudnnConv(int input_n, int input_c, int input_h, int input_w, 
+                   int k_n, int k_c, int k_h, int k_w, 
+                   int p_h, int p_w, 
+                   int s_h, int s_w,
+                   int d_h, int d_w,
+                   int group,
+                   cudnnDataType_t in_type = CUDNN_DATA_FLOAT,
+                   cudnnDataType_t weight_type = CUDNN_DATA_FLOAT,
+                   cudnnDataType_t out_type = CUDNN_DATA_FLOAT,
+                   cudnnTensorFormat_t in_format = CUDNN_TENSOR_NCHW,
+                   cudnnTensorFormat_t weight_format = CUDNN_TENSOR_NCHW,
+                   cudnnTensorFormat_t output_format = CUDNN_TENSOR_NCHW) {
+    CudnnConv conv(input_n, input_c, input_h, input_w,
+                   k_n, k_c, k_h, k_w,
+                   p_h, p_w, s_h, s_w, d_h, d_w,
+                   group,
+                   in_type, weight_type, out_type,
+                   in_format, weight_format, output_format);
+    conv.input_data(input_data_g_);
+    conv.weight_data(weight_data_g_);
+    conv.output_data(output_data_g_);
+    conv.cudnn_workspace(cudnn_workspace_g_);
+    conv.Run();
+    
+}
+
+void InitData(cudnnDataType_t type_input = CUDNN_DATA_FLOAT,
+              cudnnDataType_t type_weight = CUDNN_DATA_FLOAT,
+              cudnnDataType_t type_output = CUDNN_DATA_FLOAT) {
+    CHECK_EXIT(type_input != CUDNN_DATA_FLOAT, "only support float");
+    CHECK_EXIT(type_weight != CUDNN_DATA_FLOAT, "only support float");
+    CHECK_EXIT(type_output != CUDNN_DATA_FLOAT, "only support float");
+
+    if (!input_data_g_) cudaMalloc(&input_data_g_, kMaxDataSize);
+    if (!weight_data_g_) cudaMalloc(&weight_data_g_, kMaxWeightSize);
+    if (!output_data_g_) cudaMalloc(&output_data_g_, kMaxDataSize);
+    if (!cudnn_workspace_g_) cudaMalloc(&cudnn_workspace_g_, kMaxCudnnWorkspace_);
+
+    cudnnCreate(&cudnn_handle_g);
+}
+
+void ReleaseData() {
+    if (input_data_g_) cudaFree(input_data_g_);
+    if (weight_data_g_) cudaFree(weight_data_g_);
+    if (output_data_g_) cudaFree(output_data_g_);
+    if (cudnn_workspace_g_) cudaFree(cudnn_workspace_g_);
+    cudnnDestroy(cudnn_handle_g);
+}
 
 int main() {
     cout << "cudnn_test..........." << endl;
+    InitData();
+    TestCudnnConv(1, 32, 224, 224,
+                  128, 32, 3, 3,
+                  1, 1, 1, 1, 1, 1,
+                  1);
+    ReleaseData();
     return 0;
 }
